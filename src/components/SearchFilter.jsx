5977bd7315d3d6a1bb6b544c8c6c745a19ffd1cb
rizalyoga <ach.rizal.yogaswara@gmail.com>
2023-09-08 23:24:33 +0700

    add search component & recipe data

---
+++ src/components/SearchFilter.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Text, TextInput } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+const SearchFilter = ({ icon, placeholder }) => {
+  return (
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flexDirection: "row",
+        paddingVertical: 16,
+        borderRadius: 8,
+        paddingHorizontal: 16,
+        marginVertical: 16,
+        alignItems: "center",
+
+        // shadowColor: "#000",
+        // shadowOffset: { width: 0, height: 4 },
+        // shadowOpacity: 0.1,
+        // shadowRadius: 7,
+        elevation: 3,
+      }}
+    >
+      <FontAwesome name={icon} size={20} color="#f96163" />
+      <TextInput
+        style={{
+          paddingLeft: 8,
+          fontSize: 16,
+          color: "#808080",
+        }}
+        placeholder={placeholder}
+      ></TextInput>
+    </View>
+  );
+};
+
+export default SearchFilter;
--- src/screen/RecipeListScreen.jsx
+++ src/screen/RecipeListScreen.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import Header from "../components/Header";
+import SearchFilter from "../components/SearchFilter";
 
 const RecipeListScreen = () => {
   return (
     <SafeAreaView style={{ flex: 1, marginHorizontal: 16 }}>
       <Header headerText={"Hallo Yoga"} headerIcon={"bell-o"} />
+      <SearchFilter icon="search" placeholder="enter your favorite recipe" />
     </SafeAreaView>
   );
 };
